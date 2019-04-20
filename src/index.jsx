9c8cccee74406ae1112b09f9b34264d479e8cccf
Martin Kraft <martin@upspin.org>
2019-04-19 21:04:49 -0400

    WIP

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import { Draggable, Droppable } from '@shopify/draggable';
+import styles from '../assets/styles.css';
 
 // Courtesy of https://feathericons.com/
-const Icon = () => <i className='icon fa fa-plug'/>;
+const Icon = () => <i className='icon fa fa-plug' />;
 
 class HelloWorldPlugin {
     initialize(registry, store) {
         registry.registerPostDropdownMenuAction(
             'Move Post',
             (postID) => {
-                const post = store.getState().entities.posts.posts[postID];
-                console.log('postID', postID);
-                console.log('post', post);
+                const dropElem = document.getElementById("rhsContainer");
+                if (!dropElem) {
+                    console.log('do nothing');
+                    return;
+                }
+                const postElem = document.getElementById(`post_${postID}`); // postListContent
+                const draggable = new Draggable(postElem, {
+                    draggable: '.post__content'
+                });
+                draggable.on('drag:start', () => console.log('drag:start'));
+                draggable.on('drag:move', () => console.log('drag:move'));
+                draggable.on('drag:stop', () => console.log('drag:stop'));
+
+                // const post = store.getState().entities.posts.posts[postID];
+
+                const droppable = new Droppable(dropElem, {
+                    draggable: '.post__content'
+                });
+                droppable.on('droppable:dropped', () => console.log('droppable:dropped'));
+                droppable.on('droppable:returned', () => console.log('droppable:returned'));
             },
             (postID) => {
+                // TODO: Return false if the LHS is not open.
                 const post = store.getState().entities.posts.posts[postID];
                 if (post && post.root_id.length === 0) {
                     return true;
--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,26 @@ module.exports = {
                                 }
                             ]
                         ],
+                        plugins: [
+                            'babel-plugin-react-css-modules'
+                        ]
                     },
                 },
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: true,
+                            localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+                        },
+                    },
+                ],
             },
         ],
     },
